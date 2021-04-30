c1938942279e0eef8070515c697c80d1942e3a43
Asif Mohammad <asifratul45@gmail.com>
2021-04-30 16:00:59 +0600

    get group list added

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
 require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efdix.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
-
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(fileUpload());
 
-const port = 5000;
 
 app.get('/', (req, res) => {
     res.send("Hello from db it's working fine !!");
 })
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if(err) {
+        console.log("Error --->", err);
+        return;
+    }
+    console.log("Connected successfully");
+
+    const groupCollection = client.db(process.env.DB_NAME).collection("group");
+    const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+
+    app.post('/checkAdmin', (req, res) => {
+        const queryEmail = req.body.email;
+
+        adminCollection.find({email: queryEmail})
+        .toArray((err, document) => {
+            if(document.length)
+                res.send({adminVerified: true});
+            else
+                res.send({adminVerified: false});
+        });
+    });
+
+    app.get('/groupList', (req, res) => {
+        groupCollection.find({})
+            .toArray((err, documents) => {
+                res.status(200).send(documents);
+            });
+    });
+
+    app.post('/addGroup', (req, res) => {
+        const group_name = req.body.group_name;
+        const fb_url = req.body.fb_url;
+        const group_description = req.body.group_description;
+        const like_count = req.body.like_count;
+        const logo_image = req.files.logo;
+        console.log(group_name, fb_url, group_description, like_count, logo_image);
+
+
+        console.log("Processing Image...");
+
+        const newImg = logo_image.data;
+        const encImg = newImg.toString('base64');
+
+        const logo = {
+            contentType: logo_image.mimetype,  // mimetype = jpeg/png/jpg... etc
+            size: logo_image.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        groupCollection.insertOne({ group_name, fb_url, group_description, like_count, logo })
+            .then(result => {
+                console.log("Data sent successfully !!");
+                res.send(result.insertedCount > 0);
+            })
+    });
+});
 
 app.listen(process.env.PORT || port);
