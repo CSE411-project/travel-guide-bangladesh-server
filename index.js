c0d60a629cb0795f338727ffd41c64a656b18a2d
Asif Mohammad <asifratul45@gmail.com>
2021-06-30 01:29:39 +0600

    added destination post

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs-extra');
+const multer = require('multer');
+const imgbbUploader = require("imgbb-uploader");
+const sharp = require('sharp');
 const fileUpload = require('express-fileupload');
-require('dotenv').config();
+const fs = require('fs-extra');
+const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config();
 const port = 5000;
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '--' + file.originalname)
+    }
+});
+const upload = multer({ storage: fileStorageEngine });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efdix.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(fileUpload());
-
-
-app.get('/', (req, res) => {
-    res.send("Hello from db it's working fine !!");
-})
+// app.use(fileUpload());
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -25,26 +35,78 @@ client.connect(err => {
         console.log("Error --->", err);
         return;
     }
-    console.log("Connected successfully");
+    console.log("MongoDB connected successfully");
 
     const groupCollection = client.db(process.env.DB_NAME).collection("group");
     const destinationCollection = client.db(process.env.DB_NAME).collection("destination");
-    const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+    const userCollection = client.db(process.env.DB_NAME).collection("user");
 
-    app.post('/checkAdmin', (req, res) => {
-        const queryEmail = req.body.email;
+    app.post('/checkUser', (req, res) => {
+        const userEmail = req.body.email;
+        const userName = req.body.name;
+        const userPhoto = req.body.photo;
 
-        adminCollection.find({email: queryEmail})
+        userCollection.find({email: userEmail})
         .toArray((err, document) => {
             if(document.length)
-                res.send({adminVerified: true});
-            else
-                res.send({adminVerified: false});
+                res.status(200).send(document[0]);
+            else {
+                const user = {
+                    email: userEmail,
+                    name: userName,
+                    photo: userPhoto,
+                    isAdmin: false,
+                    liked_destinations: [],
+                    liked_groups: [],
+                    liked_guided: [],
+                    bookmarks: []
+                };
+
+                userCollection.insertOne(user)
+                    .then(result => {
+                        console.log("User info inserted successfully.")
+                        res.status(200).send(user);
+                    });
+            }
+                // res.send({adminVerified: false});
         });
+    });
+
+    app.post('/updateUserBookmark', (req, res) => {
+        userCollection.updateOne(
+            { email: req.body.email },
+            {
+                $set: { bookmarks: req.body.bookmarks }
+            }
+        )
+            .then(result => {
+                console.log(result);
+                console.log("Bookmarks updated successfully");
+            })
+    });
+
+    app.post('/updateUserLikedDestination', (req, res) => {
+        destinationCollection.updateOne(
+            { email: req.body.email },
+            {
+                $set: {liked_destinations: req.body.likedDestinations}
+            }
+        )
+            .then(result => {
+                console.log(result);
+                console.log("Liked Destination updated successfully");
+            })
     });
 
     app.get('/groupList', (req, res) => {
         groupCollection.find({})
+            .toArray((err, documents) => {
+                res.status(200).send(documents);
+            });
+    });
+
+    app.get('/destinationList', (req, res) => {
+        destinationCollection.find({})
             .toArray((err, documents) => {
                 res.status(200).send(documents);
             });
@@ -77,32 +139,64 @@ client.connect(err => {
             })
     });
 
-    app.post('/addDescription', (req, res) => {
+    app.post('/addDestination', upload.any("destinationImage"), (req, res) => {
         const destination_name = req.body.destination_name;
         const destination_district = req.body.destination_district;
         const destination_description = req.body.destination_description;
         const like_count = req.body.like_count;
-        const destinationImage = req.files.destinationImage;
-        console.log(destination_name, destination_district, destination_description, like_count, destinationImage);
+        const destinationImages = req.files;
+        
+        const getDestImage = async () => {
+            const destImage = [];
 
+            for(let i=0; i<destinationImages.length; i++) {
+                const resizeLocation = destinationImages[i].destination + "/" + Date.now() + "---" + req.files[i].originalname;
+                
+                async function resizeImage() {
+                    await sharp(destinationImages[i].path)
+                    .resize(1400, 800)
+                    .jpeg({
+                        chromaSubsampling: '4:2:0'
+                    })
+                    .withMetadata()
+                    .toFile(resizeLocation);
+                    
+                    return imgbbUploader(process.env.IMGBB_API, resizeLocation)
+                    .then((response) => {
+                        return (response.url);
+                    })
+                    .catch((error) => console.error(error));
+                }
+                
+                const imgURL = await resizeImage();
+                destImage.push(imgURL);
+            }
 
-        console.log("Processing Data...");
+            return destImage;
+        }
 
-        const newImg = destinationImage.data;
-        const encImg = newImg.toString('base64');
-
-        const destImage = {
-            contentType: destinationImage.mimetype,  // mimetype = jpeg/png/jpg... etc
-            size: destinationImage.size,
-            img: Buffer.from(encImg, 'base64')
-        };
-
-        destinationCollection.insertOne({ destination_name, destination_district, destination_description, like_count, destImage })
+        getDestImage().then(images => {
+            destinationCollection.insertOne({ destination_name, destination_district, destination_description, like_count, destImageURL: images })
             .then(result => {
-                console.log("Data sent successfully !!");
+                console.log("Destination data sent successfully !!");
                 res.send(result.insertedCount > 0);
-            })
+            });
+        });
     });
+
+    app.get('/destination/:destinationId', (req, res) => {
+        const destinationId = req.params.destinationId;
+        
+        const dest_id = new mongo.ObjectID(destinationId);
+        destinationCollection.find({'_id': dest_id})
+            .toArray((err, documents) => {
+                res.status(200).send(documents[0])
+            });
+    })
 });
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server is listening to port ${port}`);
+});
+
+
